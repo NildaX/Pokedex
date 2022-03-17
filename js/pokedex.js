f730bb2738712cc3de110d6f5a2d2dc4fe48240a
NildaX <nilda_gaby_9745@live.com.mx>
2022-03-17 11:29:24 -0600

    version final

--- js/pokedex.js
+++ js/pokedex.js
@@ -6,7 +6,17 @@ const fetchPokemon = () => {
     fetch(url).then((res) => {
         if (res.status != "200") {
             console.log(res);
-            pokeImage("/Pokedex/assets/images/pokemon-sad.jpg")
+            pokeImage("/Pokedex/assets/images/pokemon-sad.jpg");
+            (document.getElementById("name")).value="Not found";
+            (document.getElementById("numero")).value="#00";
+            (document.getElementById("habilidades")).value="--";
+            (document.getElementById("habilidadese")).value="--";
+            (document.getElementById("tipo1")).value="--";
+            (document.getElementById("tipo2")).value="--";
+            (document.getElementById("hp")).value=0;
+            (document.getElementById("attack")).value=0;
+            document.getElementById("divhp").style.width ='0px';
+            document.getElementById("divattack").style.width = '0px';
         }
         else {
             return res.json();
@@ -26,6 +36,21 @@ const fetchPokemon = () => {
             (document.getElementById("attack")).value=data['stats'][1]['base_stat'];
             document.getElementById("divhp").style.width = data['stats'][0]['base_stat']+'px';
             document.getElementById("divattack").style.width = data['stats'][1]['base_stat']+'px';
+            
+            const url2 = `https://pokeapi.co/api/v2/characteristic/${data['id']}`;
+            fetch(url2).then((res2) => {
+                if (res2.status != "200") {
+                    console.log("NOT FOUND");
+                    (document.getElementById("descripcion")).value="Characteristic not found";
+                }
+                else {
+                    return res2.json();
+                }
+                }).then((data2) => {
+                    console.log(data2);
+                    (document.getElementById("descripcion")).value=data2['descriptions'][7]['description'];
+
+            });
             try {
                 (document.getElementById("tipo2")).value=data['types'][1]['type']['name'];
                 document.getElementById("tipo2").style.display = "block";
