9ff48d7ce6fe6584947dfe99a2ee68c4ef22fac4
NildaX <nilda_gaby_9745@live.com.mx>
2022-03-16 23:04:05 -0600

    version pre-final

--- js/pokedex.js
+++ js/pokedex.js
@@ -17,6 +17,26 @@ const fetchPokemon = () => {
             let pokeImg = data.sprites.front_default;
             pokeImage(pokeImg);
             console.log(pokeImg);
+            (document.getElementById("name")).value=data['name'];
+            (document.getElementById("numero")).value="#00"+data['id'];
+            (document.getElementById("habilidades")).value=data['abilities'][0]['ability']['name'];
+            (document.getElementById("habilidadese")).value=data['abilities'][1]['ability']['name'];
+            (document.getElementById("tipo1")).value=data['types'][0]['type']['name'];
+            (document.getElementById("hp")).value=data['stats'][0]['base_stat'];
+            (document.getElementById("attack")).value=data['stats'][1]['base_stat'];
+            document.getElementById("divhp").style.width = data['stats'][0]['base_stat']+'px';
+            document.getElementById("divattack").style.width = data['stats'][1]['base_stat']+'px';
+            try {
+                (document.getElementById("tipo2")).value=data['types'][1]['type']['name'];
+                document.getElementById("tipo2").style.display = "block";
+             }
+             catch (e) {
+                document.getElementById("tipo2").style.display = "none";
+             }
+
+            
+            console.log(data['abilities'][0]['ability']['name']);
+            console.log(data['abilities'][1]['ability']['name']);
         }
     });
 }
